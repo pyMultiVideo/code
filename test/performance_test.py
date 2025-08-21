8bd4cfdcfaed91bad519eece77c668ced8c06642
Alif Aziz <55590611+alifuaziz@users.noreply.github.com>
2025-08-21 10:47:37 +0100

    Refactor performance and video processing scripts to use fire for command-line interface

--- test/performance_test.py
+++ test/performance_test.py
@@ -3,63 +3,58 @@ from pathlib import Path
 import json
 import sys
 from tqdm import tqdm
+import fire
 
-# Get the path to the test folder
-test_dir = Path(".") / "data" / "test-large"
-script_path = Path(".") / "pyMultiVideo_GUI.pyw"
-pmv_env = r"C:/Users/alifa/miniconda3/envs/pyMultiCam_env/python.exe"  # Location of pyMultiVideo Environment
 
-# List files in the test directory
-directories = [d.resolve() for d in test_dir.iterdir() if d.is_dir()]
+def run_performance_test(test_dir="data/test-large", script_path="pyMultiVideo_GUI.pyw"):
+    test_dir = Path(test_dir)
+    script_path = Path(script_path)
 
-# Calculate the approximate run time of the test
-total_runtime = 0
-for dir in directories:
-    test_path = str(dir / "test_config.json")
-    with open(test_path, "r") as f:
-        config_data = json.load(f)
-    minutes, seconds = map(int, config_data["close_after"].split(":"))
-    total_runtime += int(config_data["close_after"].replace(":", ""))
+    directories = [d.resolve() for d in test_dir.iterdir() if d.is_dir()]
 
-hours = total_runtime // 3600
-minutes = (total_runtime % 3600) // 60
-seconds = total_runtime % 60
-print(f"Approximate total runtime of the test: {hours} hours, {minutes} minutes, and {seconds} seconds")
+    total_runtime = 0
+    for dir in directories:
+        test_path = dir / "test_config.json"
+        with open(test_path, "r") as f:
+            config_data = json.load(f)
+        minutes, seconds = map(int, config_data["close_after"].split(":"))
+        total_runtime += int(config_data["close_after"].replace(":", ""))
 
-for dir in tqdm(directories, desc="Processing directories"):
-    # Get the config file paths for the experiment
-    test_path = str(dir / "test_config.json")
-    with open(test_path, "r") as f:
-        config_data = json.load(f)
+    hours = total_runtime // 3600
+    minutes = (total_runtime % 3600) // 60
+    seconds = total_runtime % 60
+    print(f"Approximate total runtime of the test: {hours} hours, {minutes} minutes, and {seconds} seconds")
 
-    # Construct the command as a list of arguments
-    command = [
-        sys.executable,  # Python executable
-        Path(".") / "pyMultiVideo_GUI.pyw",  # Path to GUI
-        # Test options specified
-        "--experiment-config",
-        json.dumps(json.dumps(config_data["experiment_config"])),  # Config file passed as JSON formatted string
-        "--camera-config",
-        json.dumps(json.dumps(config_data["camera_config"])),  # Config file passed as JSON formatted string
-        "--application-config",
-        json.dumps(json.dumps(config_data["application_config"])),  # Config file passed as JSON formatted string
-        "--record-on-startup",
-        config_data["record-on-startup"],  # Application records on startup
-        "--close-after",
-        config_data["close_after"],  # Time after which the application closes
-    ]
+    for dir in tqdm(directories, desc="Processing directories"):
+        test_path = dir / "test_config.json"
+        with open(test_path, "r") as f:
+            config_data = json.load(f)
 
-    # Join the list into a single string with spaces between each element
-    command = " ".join(map(str, command))
-    # Start the process
-    process = subprocess.Popen(
-        command, stdin=subprocess.PIPE, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP
-    )  # Ensure it runs in a new process group
-    try:
-        while process.poll() is None:
-            time.sleep(1)  # Wait for the process to finish
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    # break
+        command = [
+            sys.executable,
+            script_path,
+            "--experiment-config",
+            json.dumps(json.dumps(config_data["experiment_config"])),
+            "--camera-config",
+            json.dumps(json.dumps(config_data["camera_config"])),
+            "--application-config",
+            json.dumps(json.dumps(config_data["application_config"])),
+            "--record-on-startup",
+            config_data["record-on-startup"],
+            "--close-after",
+            config_data["close_after"],
+        ]
 
-    print(f"Finished processing directory: {dir.name}")
+        command = " ".join(map(str, command))
+        process = subprocess.Popen(command, stdin=subprocess.PIPE, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
+        try:
+            while process.poll() is None:
+                time.sleep(1)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+        print(f"Finished processing directory: {dir.name}")
+
+
+if __name__ == "__main__":
+    fire.Fire(run_performance_test)
--- test/table.py
+++ test/table.py
@@ -4,11 +4,7 @@ import json
 import pandas as pd
 import cv2
 from tqdm import tqdm
-
-test_dir = Path(".") / "data" / "test-photo-1"
-directories = [d for d in test_dir.resolve().iterdir() if d.is_dir()]
-
-camera_rows = []
+import fire
 
 
 # Flatten the testing_params dictionary
@@ -23,68 +19,74 @@ def flatten_dict(d, parent_key="", sep="_"):
     return dict(items)
 
 
-for directory in tqdm(directories, desc="Processing directories"):
-    # Load testing parameters
-    testing_params_path = directory / "test_config.json"
-    with open(testing_params_path, "r") as f:
-        testing_params = json.load(f)
+def process_videos(test_name="test-photo-1"):
+    test_dir = Path(".") / "data" / test_name
+    directories = [d for d in test_dir.resolve().iterdir() if d.is_dir()]
 
-    testing_params = flatten_dict(testing_params)
-    # Add test ID for each row
-    testing_params["test_id"] = directory.name
-    # Load metadata files
-    metadata_files = directory.glob("*metadata*.json")
-    for metadata_file in metadata_files:
-        with open(metadata_file.resolve(), "r") as f:
-            metadata = json.load(f)
+    camera_rows = []
 
-        # Extract the corresponding video file name
-        video_file_name = metadata_file.stem.replace("_metadata", "") + ".mp4"
+    for directory in tqdm(directories, desc="Processing directories"):
+        # Load testing parameters
+        testing_params_path = directory / "test_config.json"
+        with open(testing_params_path, "r") as f:
+            testing_params = json.load(f)
 
-        # Load the MP4 file path
-        video_file_path = directory / video_file_name
-        metadata["video_file_path"] = str(video_file_path.resolve())
-        # Load the MP4 file as a video
-        try:
-            video_capture = cv2.VideoCapture(str(video_file_path.resolve()))
-            if not video_capture.isOpened():
-                raise ValueError(f"Unable to open video file: {video_file_path}")
+        testing_params = flatten_dict(testing_params)
+        # Add test ID for each row
+        testing_params["test_id"] = directory.name
+        # Load metadata files
+        metadata_files = directory.glob("*metadata*.json")
+        for metadata_file in metadata_files:
+            with open(metadata_file.resolve(), "r") as f:
+                metadata = json.load(f)
 
-            # Extract video properties
-            metadata["real_frame_count"] = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
-            metadata["real_fps"] = video_capture.get(cv2.CAP_PROP_FPS)  # FPS encoded by FFMPEG
-            metadata["real_duration"] = (
-                pd.to_timedelta(metadata["real_frame_count"] / metadata["real_fps"], unit="s")
-                if metadata["real_fps"] > 0
-                else None
-            )
-        except Exception as e:
-            metadata["real_frame_count"] = None
-            metadata["real_fps"] = None
-            metadata["real_duration"] = None
-            print(f"Error processing video file {video_file_path}: {e}")
-        finally:
-            if "video_capture" in locals():
-                video_capture.release()
-        # Concatenate metadata and testing parameters
-        combined_data = {**metadata, **testing_params}
-        camera_rows.append(combined_data)
-# Create a DataFrame from the collected rows
-metadata_df = pd.DataFrame(camera_rows)
-# Create a % of dropped frames column
-metadata_df["percent_dropped_frames"] = (
-    metadata_df["dropped_frames"] / metadata_df["recorded_frames"] + metadata_df["dropped_frames"]
-) * 100
-# Reorder columns to make test_id the first column and n_cameras the second column
-columns = ["test_id"] + [col for col in metadata_df.columns if col not in ["test_id"]]
-metadata_df = metadata_df[columns]
+            # Extract the corresponding video file name
+            video_file_name = metadata_file.stem.replace("_metadata", "") + ".mp4"
 
-# Save the DataFrame as a TSV file in the test directory
-output_path = test_dir / "results.tsv"
-metadata_df.to_csv(output_path, sep="\t", index=False)
+            # Load the MP4 file path
+            video_file_path = directory / video_file_name
+            metadata["video_file_path"] = str(video_file_path.resolve())
+            # Load the MP4 file as a video
+            try:
+                video_capture = cv2.VideoCapture(str(video_file_path.resolve()))
+                if not video_capture.isOpened():
+                    raise ValueError(f"Unable to open video file: {video_file_path}")
 
-# After report generation, create a .bat script to remove video files
-bat_script_content = """@echo off
+                # Extract video properties
+                metadata["real_frame_count"] = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+                metadata["real_fps"] = video_capture.get(cv2.CAP_PROP_FPS)  # FPS encoded by FFMPEG
+                metadata["real_duration"] = (
+                    pd.to_timedelta(metadata["real_frame_count"] / metadata["real_fps"], unit="s")
+                    if metadata["real_fps"] > 0
+                    else None
+                )
+            except Exception as e:
+                metadata["real_frame_count"] = None
+                metadata["real_fps"] = None
+                metadata["real_duration"] = None
+                print(f"Error processing video file {video_file_path}: {e}")
+            finally:
+                if "video_capture" in locals():
+                    video_capture.release()
+            # Concatenate metadata and testing parameters
+            combined_data = {**metadata, **testing_params}
+            camera_rows.append(combined_data)
+    # Create a DataFrame from the collected rows
+    metadata_df = pd.DataFrame(camera_rows)
+    # Create a % of dropped frames column
+    metadata_df["percent_dropped_frames"] = (
+        metadata_df["dropped_frames"] / (metadata_df["recorded_frames"] + metadata_df["dropped_frames"])
+    ) * 100
+    # Reorder columns to make test_id the first column and n_cameras the second column
+    columns = ["test_id"] + [col for col in metadata_df.columns if col not in ["test_id"]]
+    metadata_df = metadata_df[columns]
+
+    # Save the DataFrame as a TSV file in the test directory
+    output_path = test_dir / "results.tsv"
+    metadata_df.to_csv(output_path, sep="\t", index=False)
+
+    # After report generation, create a .bat script to remove video files
+    bat_script_content = """@echo off
 setlocal
 
 :: Set the target folder (current folder by default)
@@ -100,7 +102,11 @@ echo All .mp4 files have been removed.
 pause
 """
 
-# Save the .bat script in the test directory
-bat_script_path = test_dir / "remove_videos.bat"
-with open(bat_script_path, "w") as bat_file:
-    bat_file.write(bat_script_content)
+    # Save the .bat script in the test directory
+    bat_script_path = test_dir / "remove_videos.bat"
+    with open(bat_script_path, "w") as bat_file:
+        bat_file.write(bat_script_content)
+
+
+if __name__ == "__main__":
+    fire.Fire(process_videos)
